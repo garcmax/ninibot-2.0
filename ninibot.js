ab4e28d6c75962d20caabd6119698c4c7931a687
Maximilien Garcia <maximilien.garcia@gmail.com>
2016-09-12 16:22:34 +0200

    creation of credentials and tests folders

---
+++ ninibot.js
@@ -0,0 +1,8 @@
+'strict';
+
+// import the discord.js module
+const Discord = require('discord.js');
+
+// create an instance of a Discord Client, and call it bot
+const bot = new Discord.Client();
+
---
+++ src/config/credentials.js
@@ -0,0 +1,5 @@
+export const credentials = {
+  "googleToken" : process.env.NODE_GOOGLE_API_TOKEN,
+  "imgurId" : "Client-ID " + process.env.NODE_IMGUR_CLIENT_ID,  
+  "discordToken" : process.env.NODE_NINIBOT_TOKEN
+};
